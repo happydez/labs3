9c776e0181b17be19cfa4778a32de5828d3eacc5
happydez <happy_dez@outlook.com>
2023-12-09 00:32:36 +0300

    AVL & Trie tree completed!

---
+++ M/tree/AVL_tree/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+#include "AVLTree.h"
+
+using namespace std;
+
+int main() {
+
+    AVLTree tree;
+
+    tree.Insert(12);
+    tree.Insert(6);
+    tree.Insert(30);
+    tree.Insert(45);
+    tree.Insert(60);
+
+    tree.Print();
+
+    if (tree.Search(12)) {
+        cout << "12 in tree" << endl;
+    }
+    else {
+        cout << "12 isn't in tree" << endl;
+    }
+
+    tree.Remove(12);
+
+    cout << "remove 12 from tree" << endl;
+
+    if (tree.Search(12)) {
+        cout << "12 in tree" << endl;
+    }
+    else {
+        cout << "12 isn't in tree" << endl;
+    }
+
+    tree.Print();
+
+    return 0;
+}
---
+++ M/tree/Trie_tree/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+#include <Windows.h>
+
+#include "TrieTree.h"
+
+using namespace std;
+
+int main() {
+
+	TrieTree tree = TrieTree();
+
+	tree.Insert("cat");
+	tree.Insert("cats");
+	tree.Insert("calculator");
+	tree.Insert("house");
+	tree.Insert("cow");
+
+	tree.Show();
+
+	tree.Remove("cat");
+
+	cout << endl;
+	cout << string(32, '-') << endl;
+	cout << endl;
+
+	tree.Show();
+
+	return 0;
+}
