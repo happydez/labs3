df89468ebaf6622f93a5484b1d3d721eb8809a55
happydez <happy_dez@outlook.com>
2023-12-18 21:09:38 +0300

    lab2

---
+++ M/tree/Trie_tree/lab2/TrieTree.h
@@ -0,0 +1,166 @@
+#pragma once
+
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class TrieTree {
+
+private:
+
+	static const int AN = 26;
+
+	struct TrieTreeNode {
+		TrieTreeNode* children[AN];
+		bool isTerminal;
+
+		TrieTreeNode() {
+			for (int i = 0; i < AN; i++) {
+				children[i] = nullptr;
+			}
+
+			isTerminal = false;
+		}
+	};
+
+	TrieTreeNode* root;
+
+	void insertHelper(TrieTreeNode* root, string& word) {
+		TrieTreeNode* currentNode = root;
+
+		for (int i = 0; i < word.length(); i++) {
+			int index = tolower(word[i]) - 'a';
+
+			if (currentNode->children[index] == nullptr) {
+				currentNode->children[index] = new TrieTreeNode();
+			}
+
+			currentNode = currentNode->children[index];
+		}
+
+		currentNode->isTerminal = true;
+	}
+
+	TrieTreeNode* searchHelper(TrieTreeNode* root, string& word) {
+		TrieTreeNode* currentNode = root;
+
+		for (int i = 0; i < word.length(); i++) {
+			int index = tolower(word[i]) - 'a';
+
+			if (currentNode->children[index] == nullptr) {
+				break;
+			}
+
+			currentNode = currentNode->children[index];
+		}
+
+		return currentNode;
+	}
+
+	void removeHelper(TrieTreeNode* root, string& word) {
+		if (searchHelper(root, word)) {
+			TrieTreeNode* currentNode = root;
+
+			for (int i = 0; i < word.length(); i++) {
+				int index = tolower(word[i]) - 'a';
+				currentNode = currentNode->children[index];
+			}
+
+			currentNode->isTerminal = false;
+		}
+	}
+
+	void inorder(TrieTreeNode* root, string wseq, queue<string>& words) {
+		if (root != nullptr) {
+			for (int i = 0; i < AN; i++) {
+				if (root->children[i] != nullptr) {
+					if (root->children[i]->isTerminal) {
+						words.push(wseq + string(1, 'a' + i));
+					}
+
+					inorder(root->children[i], wseq + string(1, 'a' + i), words);
+				}
+			}
+		}
+	}
+
+	void deleteWithSubstr(TrieTreeNode* root, string ss) {
+		queue<string> words;
+
+		for (int i = 0; i < AN; i++) {
+			if (root->children[i] != nullptr) {
+				inorder(root->children[i], string(1, 'a' + i), words);
+			}
+		}
+
+		TrieTreeNode* token = nullptr;
+		while (!words.empty()) {
+			string next = words.front();
+			words.pop();
+
+			token = searchHelper(root, next);
+			if ((token != nullptr) && (next.find(ss) != string::npos) && (token->isTerminal)) {
+				token->isTerminal = false;
+			}
+		}
+	}
+
+	void inorderHelper(TrieTreeNode* root) {
+		queue<string> words;
+
+		for (int i = 0; i < AN; i++) {
+			if (root->children[i] != nullptr) {
+				inorder(root->children[i], string(1, 'a' + i), words);
+			}
+		}
+
+		while (!words.empty()) {
+			cout << words.front() << endl;
+			words.pop();
+		}
+	}
+
+	void deleteHelper(TrieTreeNode* root) {
+		if (root != nullptr) {
+			for (int i = 0; i < AN; i++) {
+				if (root->children[i] != nullptr) {
+					deleteHelper(root->children[i]);
+					delete root->children[i];
+				}
+			}
+		}
+	}
+
+public:
+
+	TrieTree() {
+		root = new TrieTreeNode();
+	}
+
+	~TrieTree() {
+		deleteHelper(root);
+	}
+
+	void Insert(string word) {
+		insertHelper(root, word);
+	}
+
+	void Remove(string word) {
+		removeHelper(root, word);
+	}
+
+	void RemoveWithSubstr(string ss) {
+		deleteWithSubstr(root, ss);
+	}
+
+	bool Search(string word) {
+		return searchHelper(root, word)->isTerminal;
+	}
+
+	void Show() {
+		inorderHelper(root);
+	}
+};
---
+++ M/tree/Trie_tree/lab2/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+#include "TrieTree.h"
+
+using namespace std;
+
+int main() {
+    
+    TrieTree tree;
+
+    tree.Insert("okey");
+    tree.Insert("sok");
+    tree.Insert("okgg");
+    tree.Insert("fff");
+
+    cout << "Before: " << endl;
+    tree.Show();
+    cout << endl << endl;
+
+    tree.RemoveWithSubstr("ok");
+
+    cout << "After: " << endl;
+    tree.Show();
+    cout << endl << endl;
+
+    return 0;
+}
